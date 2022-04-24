5f341bd98ddb51dc69e87b3ac72c59452d2fba46
mhd-rawashdah <mhd.rawashdah@gmail.com>
2022-04-24 07:00:55 +0300

    increase limit

--- client/src/app/features/tutors/tutors.component.ts
+++ client/src/app/features/tutors/tutors.component.ts
@@ -19,7 +19,7 @@ export class TutorsComponent implements OnInit, OnDestroy {
   notscrolly = false;
   search: string = "";
   page = 1;
-  limit = 5;
+  limit = 20;
 
   subscriptions: Subscription[] = [];
   constructor(
